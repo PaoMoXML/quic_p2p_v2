dcb1b6eca997df1d898808d5fe0076eac021dc57
xumenglin <xml001001@126.com>
2025-12-31 17:01:29 +0800

    feat: 添加了serde依赖并添加了serde的feature

---
+++ plumtree/src/system.rs
@@ -0,0 +1,14 @@
+use std::hash::Hash;
+
+/// This trait allows for defining a system to which Plumtree nodes belong.
+
+pub trait System {
+    /// Node identifier.
+    type NodeId: Clone + Hash + Eq;
+
+    /// Message identifier.
+    type MessageId: Clone + Hash + Eq;
+
+    /// Message payload.
+    type MessagePayload: Clone;
+}
